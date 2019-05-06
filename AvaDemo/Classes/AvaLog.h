d971eaeba52a59007fd5f00279d8c52fe935d3f8
Avalanching <18269285634@163.com>
2019-05-06 09:36:15 +0800

    init

---
+++ AvaDemo/Classes/AvaLog.h
@@ -0,0 +1,17 @@
+//
+//  AvaLog.h
+//  AvaDemo_Example
+//
+//  Created by Avalanching on 2019/5/5.
+//  Copyright © 2019 avanlanching. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AvaLog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AvaDemo/Classes/AvaLog.m
@@ -0,0 +1,17 @@
+//
+//  AvaLog.m
+//  AvaDemo_Example
+//
+//  Created by Avalanching on 2019/5/5.
+//  Copyright © 2019 avanlanching. All rights reserved.
+//
+
+#import "AvaLog.h"
+
+@implementation AvaLog
+
++ (void)load {
+    NSLog(@"hello world");
+}
+
+@end
